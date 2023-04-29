d3af1f0bc1404e501bf01965b5d67eecafeddcfb
Ibrahim07ai <86628366+Ibrahim07ai@users.noreply.github.com>
2023-04-29 15:47:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let menu = document.querySelector('#menu');
 let nav = document.querySelector('.nav_links');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx')
-    nav.classList.toggle('open')
+    menu.classList.toggle("fa-solid fa-bars")
+nav.classList.toggle('open')
 }
